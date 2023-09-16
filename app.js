2f64baf035769ca614da27ba87c40e65b12db020
VazquezAlex <alejandrovazquez.dev@gmail.com>
2023-09-16 11:44:16 -0600

    feature: get all the tours

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
+// Core modules.
+const fs = require('fs');
+
+// Third party modules.
 const express = require('express');
 
+// Create the app.
 const app = express();
+
+// Extract the data for the tours.
+const tours = JSON.parse(fs.readFileSync(`${ __dirname }/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: { 
+            tours: tours
+        }
+    });
+});
 
 const port = 3000;
 app.listen(port, () => {
