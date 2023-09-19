37f8ec41a9f099ca42dfdadb424cbb9fd819558b
VazquezAlex <alejandrovazquez.dev@gmail.com>
2023-09-19 12:37:35 -0600

    feature: add connection to mongo

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 // Third party modules.
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 
 // Local imports
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+// Connect to mongo.
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+}).then(con => {
+    console.log(con.connections);
+    console.log('DB Connection Successful');
+});
 
 // Server 👇🏻.
 const port = process.env.PORT || 3000;
