1399718dccfb2d2a0a8c0812f95cd060ddc93e5c
VazquezAlex <alejandrovazquez.dev@gmail.com>
2023-09-20 10:44:04 -0600

    feature: added Model and save method as test

--- server.js
+++ server.js
@@ -19,6 +19,37 @@ mongoose.connect(DB, {
     console.log('DB Connection Successful');
 });
 
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "A tour must have a name."],
+        unique: true
+    },
+    price: {
+        type: Number,
+        required: [true, "A tour must have a price."],
+    },
+    rating: {
+        type: Number,
+        default: 4.5,
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+    name: 'La Muralla',
+    rating: 4.7,
+    price: 497
+});
+
+// Commented to not re-save this on each run.
+// testTour.save().then((doc) => {
+//     console.log(doc)
+// }).catch(e => {
+//     console.log('Error 🐛: ' + e);
+// });
+
 // Server 👇🏻.
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
