769881fff6f1477f789585ce026943d49c48fd6b
VazquezAlex <alejandrovazquez.dev@gmail.com>
2023-09-26 13:22:40 -0600

    feature: updated the tour model

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -77,7 +77,7 @@ const createTour = async (req, res) => {
     } catch (err) {
         res.status(400).json({
             status: 'failed',
-            message: 'Invalid data sent!'
+            message: 'Invalid data sent! ' + err
         });
     }
 }
--- models/tourModels.js
+++ models/tourModels.js
@@ -4,16 +4,59 @@ const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, "A tour must have a name."],
-        unique: true
+        unique: true,
+        trim: true
+    },
+    duration: {
+        type: Number,
+        required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a difficulty'],
     },
     price: {
         type: Number,
         required: [true, "A tour must have a price."],
     },
-    rating: {
+    summary: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image.'],
+    },
+    startDates: {
+        type: [Date],
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    images: {
+        type: [String]
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+    priceDiscount: {
+        type: Number,
+    },
+    ratingsAverage: {
         type: Number,
         default: 4.5,
-    }
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0,
+    },
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
