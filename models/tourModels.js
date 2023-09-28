c5d9438235b2b6fbce1a644afef21ff6264fcb2e
VazquezAlex <alejandrovazquez.dev@gmail.com>
2023-09-28 09:54:53 -0600

    feature: added custom validatorsg

--- models/tourModels.js
+++ models/tourModels.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -8,7 +9,8 @@ const tourSchema = new mongoose.Schema({
         unique: true,
         trim: true,
         maxLength: [40, 'A tour name must have less or equal than 40 characters.'],
-        minLength: [10, 'A tour name must have more or equal than 10 characters.']
+        minLength: [10, 'A tour name must have more or equal than 10 characters.'],
+        // validate: [validator.isAlpha, 'Tour name must only contain characters']
     },
     duration: {
         type: Number,
@@ -56,6 +58,13 @@ const tourSchema = new mongoose.Schema({
     },
     priceDiscount: {
         type: Number,
+        validate: {
+            validator: function(val) {
+                // this only points to current doc on NEW document creation.
+                return val < this.price;
+            },
+            message: 'Discount price ({VALUE}) should be below the regular price'
+        }
     },
     ratingsAverage: {
         type: Number,
