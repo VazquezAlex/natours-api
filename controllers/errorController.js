c5fc67cc0b3a4bf112308fe69ad09e8f1c5b06cb
VazquezAlex <alejandrovazquez.dev@gmail.com>
2023-09-28 12:17:12 -0600

    feature: added missing data and duplicated fields error handler

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,25 +1,27 @@
+// Local imports.
+const AppError = require("../utils/appError");
 
-const sendErrorDev = (error, res) => {
-    res.status(error.statusCode).json({
-        status: error.status,
-        error: error,
-        message: error.message,
-        stack: error.stack
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
     });
 }
 
-const sendErrorProd = (error, res) => {
+const sendErrorProd = (err, res) => {
     // Operation trusted error, we send the message.
-    if (error.isOperational) {
-        res.status(error.statusCode).json({
-            status: error.status,
-            message: error.message,
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
         });
 
     // Unknown error, we don't want to leak the details to the client.
     } else {
         // Log the error for the developers.
-        console.log('ERROR 💥' + error);
+        console.log('ERROR 💥' + err);
 
         // We send the client a generic messsage.
         res.status(500).json({
@@ -29,14 +31,33 @@ const sendErrorProd = (error, res) => {
     }
 }
 
-const appErrorController = (error, req, res, next) => {
-    error.statusCode = error.statusCode || 500;
-    error.status = error.status || 'error';
+const handleCastErrorDB = (err) => {
+    const message = `Invalid ${ err.path }: ${ err.value }`;
+
+    
+    return new AppError(message, 400);
+}
+
+const handleDuplicateFieldsDB = (err) => {
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    const message = `Duplicate filed value: ${ value }. Please use another value.`;
+
+    return new AppError(message, 400);
+}
+
+const appErrorController = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
 
     if (process.env.NODE_ENV === 'development') {
-        sendErrorDev(error, res);
+        sendErrorDev(err, res);
     }
     if (process.env.NODE_ENV === 'production') {
+        let error = Object.assign(err);
+        
+        if (err.name === 'CastError') error = handleCastErrorDB(error);
+        if (err.code === 11000) error = handleDuplicateFieldsDB(error);
+
         sendErrorProd(error, res);
     }
 
