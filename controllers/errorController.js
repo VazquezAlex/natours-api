b7d0598222575ce8edd09902ed132d50b9d00621
VazquezAlex <alejandrovazquez.dev@gmail.com>
2023-09-28 12:00:28 -0600

    feature: handle the errors on prod and dev

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,12 +1,45 @@
 
+const sendErrorDev = (error, res) => {
+    res.status(error.statusCode).json({
+        status: error.status,
+        error: error,
+        message: error.message,
+        stack: error.stack
+    });
+}
+
+const sendErrorProd = (error, res) => {
+    // Operation trusted error, we send the message.
+    if (error.isOperational) {
+        res.status(error.statusCode).json({
+            status: error.status,
+            message: error.message,
+        });
+
+    // Unknown error, we don't want to leak the details to the client.
+    } else {
+        // Log the error for the developers.
+        console.log('ERROR 💥' + error);
+
+        // We send the client a generic messsage.
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went very wrong!'
+        })
+    }
+}
+
 const appErrorController = (error, req, res, next) => {
     error.statusCode = error.statusCode || 500;
     error.status = error.status || 'error';
 
-    res.status(error.statusCode).json({
-        status: error.status,
-        message: error.message
-    });
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(error, res);
+    }
+    if (process.env.NODE_ENV === 'production') {
+        sendErrorProd(error, res);
+    }
+
 }
 
 module.exports = appErrorController;
