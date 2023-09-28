17fca40c1f43ed91ba49ece5ab2ab1e1bbd3da2e
VazquezAlex <alejandrovazquez.dev@gmail.com>
2023-09-28 09:45:02 -0600

    feature: added built-in validators

--- models/tourModels.js
+++ models/tourModels.js
@@ -6,7 +6,9 @@ const tourSchema = new mongoose.Schema({
         type: String,
         required: [true, "A tour must have a name."],
         unique: true,
-        trim: true
+        trim: true,
+        maxLength: [40, 'A tour name must have less or equal than 40 characters.'],
+        minLength: [10, 'A tour name must have more or equal than 10 characters.']
     },
     duration: {
         type: Number,
@@ -19,6 +21,10 @@ const tourSchema = new mongoose.Schema({
     difficulty: {
         type: String,
         required: [true, 'A tour must have a difficulty'],
+        enum: {
+            values: ['easy', 'medium', 'difficult'],
+            message: 'Difficulty is either easy, medium or difficult'
+        }
     },
     price: {
         type: Number,
@@ -54,6 +60,8 @@ const tourSchema = new mongoose.Schema({
     ratingsAverage: {
         type: Number,
         default: 4.5,
+        min: [1, 'Rating must be above 1.0'],
+        max: [5, 'Rating must be below 5.0']
     },
     ratingsQuantity: {
         type: Number,
