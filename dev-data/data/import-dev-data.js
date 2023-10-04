f586692f9d6ff87e22e1b54a0b1fc726d5e00dff
VazquezAlex <alejandrovazquez.dev@gmail.com>
2023-10-04 12:23:21 -0600

    feature: updated data import script

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -7,6 +7,8 @@ const mongoose = require('mongoose');
 
 // Local imports.
 const Tour = require('./../../models/tourModels');
+const Review = require('./../../models/reviewModel');
+const User = require('./../../models/userModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -24,11 +26,15 @@ mongoose.connect(DB, {
 
 // Get the json file data.
 const tours = JSON.parse(fs.readFileSync(`${ __dirname }/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${ __dirname }/users.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${ __dirname }/reviews.json`, 'utf-8'));
 
 // Import data into DB.
 const importData = async () => {
     try {
         await Tour.create(tours);
+        await User.create(users, { validateBeforeSave: false });
+        await Review.create(reviews, { validateBeforeSave: false });
         console.log('Data successfully loaded');
         process.exit();
     } catch (e) {
@@ -40,6 +46,8 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Tour.deleteMany();
+        await User.deleteMany();
+        await Review.deleteMany();
         console.log('Data successfully deleted');
         process.exit();
     } catch (e) {
