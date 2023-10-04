f79bb939767e16581391c2ca65a3adaee2e9f8fc
VazquezAlex <alejandrovazquez.dev@gmail.com>
2023-10-04 14:04:56 -0600

    feature: calculate the average rating when updating or adding a review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModels');
 
 const reviewSchema = new mongoose.Schema({
     review: {
@@ -37,6 +38,55 @@ reviewSchema.pre(/^find/, function(next) {
 
     next();
 });
+
+// Function to calculate the average ratings of a tour.
+reviewSchema.statics.calcAverageRatings = async function(tourId) {
+    // Here, 'this' equals to the current model.
+    const stats = await this.aggregate([
+        {
+            $match: { tour: tourId }
+        },
+        {
+            $group: {
+                _id: '$tour',
+                nRating: { $sum: 1 },
+                avgRating: {$avg: '$rating' }
+            }
+        }
+    ]);
+
+    if (stats.length > 0) {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsAverage: stats[0].avgRating,
+            ratingsQuantity: stats[0].nRating,
+        });
+    } else {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsAverage: 0,
+            ratingsQuantity: 0,
+        });
+    }
+
+};
+
+// Middleware to calcular the ratings average, after saving a new review for a tour.
+reviewSchema.post('save', function() {
+    // 'this' points to current review.
+    this.constructor.calcAverageRatings(this.tour);
+
+});
+
+reviewSchema.pre(/^findOneAnd/, async function(next) {
+    this.r = await this.findOne();
+
+    next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function() {
+    await this.r.constructor.calcAverageRatings(this.r.tour);
+});
+
+
 
 const Review = mongoose.model('Review', reviewSchema);
 
