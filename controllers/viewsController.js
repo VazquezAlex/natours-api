051c21b189595efa20ec4f89be7709ed0e72b4f1
VazquezAlex <alejandrovazquez.dev@gmail.com>
2023-10-11 15:07:11 -0600

    feature: added the overview page with data loading

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,9 +1,21 @@
+// Local imports.
+const catchAsync = require('../utils/catchAsync');
+const Tour = require('./../models/tourModels');
 
-const getOverview = (req, res) => {
+const getOverview = catchAsync(async (req, res, next) => {
+
+    // 1) Get tour data from collection.
+    const tours = await Tour.find();
+
+    // 2) Build template
+
+    // 3) Render the template using tour data from 1).
+
     res.status(200).render('overview', {
-        title: 'All Tours'
+        title: 'All Tours',
+        tours
     });
-}
+});
 
 const getTour = (req, res) => {
     res.status(200).render('tour', {
